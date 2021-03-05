883cb5b84a2df842d6b068f3b792d99827e4afbc
Dev-Jain04 <dakshesh.jain14@gmail.com>
2021-03-05 19:52:30 +0530

    changed client to icontains

--- manager/filters.py
+++ manager/filters.py
@@ -7,13 +7,13 @@ from .models import SellsReport, ExpensesIncomeModel
 class SellsReportFilter(django_filters.FilterSet):
     amount_start = django_filters.NumberFilter(field_name="total_price",lookup_expr="gte")
     amount_end = django_filters.NumberFilter(field_name="total_price",lookup_expr="lte")
+    client = django_filters.CharFilter(lookup_expr='icontains')
     class Meta:
         model = SellsReport
         fields = [
             'client',
             'payment_completed',
         ]
-
 
 class MoneyFilter(django_filters.FilterSet):
     class Meta:
