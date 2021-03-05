d99c71830f2b53607dba7bbfbf3bd289f12f7382
Dev-Jain04 <dakshesh.jain14@gmail.com>
2021-03-05 22:14:25 +0530

    changed admin panal

--- manager/admin.py
+++ manager/admin.py
@@ -1,29 +1,29 @@
 from django.contrib import admin
 
-from .models import ExpensesIncomeModel, Size, PaidAmount, SellsReport
+# from .models import ExpensesIncomeModel, Size, PaidAmount, SellsReport
 
-class InLineSizeItemSold(admin.TabularInline):
-    model = Size
+# class InLineSizeItemSold(admin.TabularInline):
+#     model = Size
 
-class InLinePaidAmountItemSold(admin.TabularInline):
-    model = PaidAmount
+# class InLinePaidAmountItemSold(admin.TabularInline):
+#     model = PaidAmount
 
 
-class SellsReportAdmin(admin.ModelAdmin):
-    inlines = [InLineSizeItemSold,InLinePaidAmountItemSold]
-    list_display = ('client','balance','payment_completed',)
-    list_per_page = 15
-    search_fields = ('client',)
-    list_filter = ('payment_completed',)
-    # readonly_fields = ('date',)
+# class SellsReportAdmin(admin.ModelAdmin):
+#     inlines = [InLineSizeItemSold,InLinePaidAmountItemSold]
+#     list_display = ('client','balance','payment_completed',)
+#     list_per_page = 15
+#     search_fields = ('client',)
+#     list_filter = ('payment_completed',)
+#     # readonly_fields = ('date',)
 
-class PaidAmountAdmin(admin.ModelAdmin):
-    readonly_fields = ('date',)
+# class PaidAmountAdmin(admin.ModelAdmin):
+#     readonly_fields = ('date',)
 
-class ExpensesIncomeModelAdmin(admin.ModelAdmin):
-    readonly_fields = ('date',)
+# class ExpensesIncomeModelAdmin(admin.ModelAdmin):
+#     readonly_fields = ('date',)
 
-admin.site.register(SellsReport,SellsReportAdmin)
-admin.site.register(Size)
-admin.site.register(ExpensesIncomeModel,ExpensesIncomeModelAdmin)
-admin.site.register(PaidAmount,PaidAmountAdmin)
+# admin.site.register(SellsReport,SellsReportAdmin)
+# admin.site.register(Size)
+# admin.site.register(ExpensesIncomeModel,ExpensesIncomeModelAdmin)
+# admin.site.register(PaidAmount,PaidAmountAdmin)
